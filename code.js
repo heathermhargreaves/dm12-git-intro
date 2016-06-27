ff70508133e5aadd4db39a9bfd6b28f8f579611a
heathermhargreaves <heathermhargreaves@gmail.com>
2016-06-27 14:42:39 -0600

    new journal entry

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
 var journal = ["So much fun!", "Reddit is great too for learning"];
 
 journal.push("Wow");
+
+/*
+git clone [URL]
+git status
+git add fileName
+git commit -m "your message"
+git push
+
+git remote -v
+*/

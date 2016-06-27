9392a59843c8d908917114f55561cddaf34a1cdf
heathermhargreaves <heathermhargreaves@gmail.com>
2016-06-27 14:45:50 -0600

    yet another journal entry

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-var journal = ["So much fun!", "Reddit is great too for learning", "Hope I can use the Twilio API after this"];
+var journal = ["So much fun!", "Reddit is great too for learning", "Hope I can use the Twilio API after this", "oh happy days"];
 
 journal.push("Wow");
 
